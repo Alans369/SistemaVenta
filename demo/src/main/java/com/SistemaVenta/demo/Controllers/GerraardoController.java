dcbbe0c5a0c474b26aa22165603013a81436eae9
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-07-29 11:41:25 -0600

    controller gerrardo

---
+++ demo/src/main/java/com/SistemaVenta/demo/Controllers/GerraardoController.java
@@ -0,0 +1,19 @@
+package com.SistemaVenta.demo.Controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/gerardo")
+public class GerraardoController {
+
+  
+    @GetMapping("/layout")
+    public String layout() {
+        return "layouts/_main_layout";
+    }
+
+}
