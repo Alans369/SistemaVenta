ff665495f8b779dfc4d26ed743f9259fbae1964e
Gerardo Antonio Barrientos Salazar <bs25001@esfe.agape.edu.sv>
2025-07-31 08:58:51 -0600

    cambios

--- demo/src/main/java/com/SistemaVenta/demo/Config/SecurityConfig.java
+++ demo/src/main/java/com/SistemaVenta/demo/Config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.SistemaVenta.demo.Config;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
