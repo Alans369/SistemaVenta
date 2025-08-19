3536e8a19bf6ea2384203bbb0897f07534398a1e
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-08-19 08:32:59 -0600

    cambios en el controlador

--- demo/src/main/java/com/SistemaVenta/demo/Controllers/AdminController.java
+++ demo/src/main/java/com/SistemaVenta/demo/Controllers/AdminController.java
@@ -7,14 +7,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.SistemaVenta.demo.Model.Product;
 import com.SistemaVenta.demo.Services.Implementation.ProductService;
+import com.SistemaVenta.demo.Utils.Util;
 
-import org.springframework.ui.Model;
+import jakarta.servlet.http.HttpServletRequest;
 
 
 
@@ -26,15 +28,20 @@ public class AdminController {
     private ProductService productService;
 
     @GetMapping("/admin")
-    public String admin1( Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
+    public String admin1( Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size, HttpServletRequest request) {
         int currentPage = page.orElse(1)-1; // si no está seteado se asigna 0
         int pageSize = size.orElse(5); // tamaño de la página, se asigna 5
         Pageable pageable = PageRequest.of(currentPage, pageSize);
         String nombre = null;
         Integer categoriaId = null;
-        String marca = "6";
+         Integer marcaId = null;
+         String marca = Util.extractTokenFromCookie(request,"marca");
+         System.out.println("Marca extraída de la cookie: " + marca);
+         if (marca != null) {
+             marcaId = Integer.parseInt(marca);
+         }
 
-       Page<Product> productos = productService.searchBynameOrCategory(nombre, categoriaId, Integer.parseInt(marca), pageable);
+       Page<Product> productos = productService.searchBynameOrCategory(nombre, categoriaId, marcaId, pageable);
 
        model.addAttribute("productos", productos);
 
